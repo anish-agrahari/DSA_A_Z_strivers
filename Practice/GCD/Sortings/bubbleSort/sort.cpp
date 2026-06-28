5646dc84f5e670f40a7079d08eb162426bb5b573
Anish Agrahari Rajnarayan <163569444+anish-agrahari@users.noreply.github.com>
2026-06-28 22:26:53 +0530

    Add files via upload

---
+++ Practice/GCD/Sortings/bubbleSort/sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for(int i=0; i<n-1; i++){
+        bool isSwap = false;
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                isSwap=true;
+            }
+        }    
+        if(!isSwap)
+        break;
+    }
+}
+
+int main (){
+    int n;
+    cin >>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    bubbleSort(arr, n);
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
