86dceae701a4e81214f5e9f5cf9851a4f6b67029
Anish Agrahari Rajnarayan <163569444+anish-agrahari@users.noreply.github.com>
2026-08-22 22:55:45 +0530

    Time: 10 ms (99.52%) | Memory: 70.6 MB (12.48%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
-    int count(vector<int>& bloomDay, int mid, int m, int k){
-        int n=bloomDay.size();
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(bloomDay[i]<=mid)
-            count++;
-            else count=0;
-            if(count==k){
-                count=0;
-                m--;
-            }
-            if(m<=0) return 1;
-        }
-        return 0;
-    }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n=bloomDay.size();
-        if((long long)m*k>n) return -1;
-        int low=INT_MAX, high=0;
-        for(int i=0;i<n;i++){
-            low=min(low,bloomDay[i]);
-            high=max(high,bloomDay[i]);
-        }
-        int ans=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            int possible=count(bloomDay,mid,m,k);
-            if(possible==1){
-                ans=mid;
-                high=mid-1;
+        int n = bloomDay.size();
+        if((long long)m*k> bloomDay.size()) return -1;
+
+        int maxi = INT_MIN;
+        for(int i = 0;i < n;i++) maxi = max(maxi,bloomDay[i]);
+
+        int low = 0, high = maxi;
+        while(low <= high){
+            int mid = low + (high-low)/2;//no days passed
+
+            int cnt = 0,bq=0;
+            for(int i = 0; i < n;i++){
+                if(bloomDay[i]<=mid){
+                    cnt++;
+                }else cnt = 0;
+                if(cnt==k){
+                    bq++;
+                    cnt = 0;
+                }
             }
-            else low=mid+1;
+            
+            if(bq>=m) high = mid - 1;
+            else low = mid + 1;
         }
-        return ans;
+        return low;
     }
 };
